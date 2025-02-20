14a63fd41d5c1f2649052907714cc6ce884cbda2
roman <roman2027@internet.ru>
2025-02-20 10:57:36 +0600

    refactor: code refs: https://github.com/RonasIT/laravel-telescope-extension/issues/5

--- src/Filters/AbstractFilter.php
+++ src/Filters/AbstractFilter.php
@@ -27,33 +27,33 @@ abstract class AbstractFilter
 
     protected function isFailedRequest(IncomingEntry $entry): bool
     {
-        $currentStatus = $entry->content['response_status'] ?? Response::HTTP_OK;
-
-        return !$this->isIgnorableRequest($entry) && $currentStatus >= Response::HTTP_BAD_REQUEST;
-    }
-
-    protected function isIgnorableRequest(IncomingEntry $entry): bool
-    {
-        return !$entry->isRequest()
-            || $this->hasIgnorableMessage($entry->content);
+        return $entry->isRequest()
+            && $this->hasFailedStatus($entry)
+            && !$this->hasIgnorableMessage($entry->content);
     }
 
     protected function hasIgnorableMessage(array $content): bool
     {
         $errorMessage = Arr::get($content, 'response.message');
-        $ignorableMessages = config('telescope.watchers.' . RequestWatcher::class . 'ignore_error_messages', []);
+        $ignorableMessages = Arr::get($this->requestWatcherConfig, 'ignore_error_messages', []);
 
         return in_array($errorMessage, $ignorableMessages);
     }
 
     protected function isFailedHttpRequest(IncomingEntry $entry): bool
     {
-        return $entry->type === EntryType::CLIENT_REQUEST
-            && ($entry->content['response_status'] ?? Response::HTTP_OK) >= Response::HTTP_BAD_REQUEST;
+        return $entry->isClientRequest() && $this->hasFailedStatus($entry);
     }
 
     protected function isJob(IncomingEntry $entry): bool
     {
         return $entry->type === EntryType::JOB;
+    }
+
+    protected function hasFailedStatus(IncomingEntry $entry): bool
+    {
+        $currentStatus = $entry->content['response_status'] ?? Response::HTTP_OK;
+
+        return $currentStatus >= Response::HTTP_BAD_REQUEST;
     }
 }
--- src/Filters/ProductionFilter.php
+++ src/Filters/ProductionFilter.php
@@ -3,13 +3,15 @@
 namespace RonasIT\TelescopeExtension\Filters;
 
 use Closure;
+use Illuminate\Support\Facades\App;
 use Laravel\Telescope\IncomingEntry;
 
-class PRODFilter extends AbstractFilter
+class ProductionFilter extends AbstractFilter
 {
     public function apply(): Closure
     {
-        return fn (IncomingEntry $entry) => $this->isException($entry)
+        return fn (IncomingEntry $entry) => App::environment('local', 'development')
+            || $this->isException($entry)
             || $this->isFailedRequest($entry)
             || $this->isFailedHttpRequest($entry)
             || $entry->isSlowQuery()
