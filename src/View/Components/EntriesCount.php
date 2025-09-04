335855ab69ce7cdd60b10ee9c145cde258467fa6
artosepyan <artyomosepyan@mail.ru>
2025-09-04 19:32:47 +0300

    fix: remarks from reviewer

--- src/View/Components/EntriesCount.php
+++ src/View/Components/EntriesCount.php
@@ -8,16 +8,18 @@ use Illuminate\View\Component;
 class EntriesCount extends Component
 {
     public string $type;
+    public string $label;
 
-    public function __construct(string $type)
+    public function __construct(string $type, string $label)
     {
         $this->type = $type;
+        $this->label = $label;
     }
 
     public function render()
     {
         $count = DB::table('telescope_entries')->where('type', $this->type)->count();
 
-        return $count > 0 ? "({$count})" : '';
+        return ($count > 0) ? "{$this->label} ({$count})" : $this->label;
     }
 }
